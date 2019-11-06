fdcb36b28d42912456d4d334e3303c76b902b9c3
Luis Egan <luisdegan10@hotmail.com>
2019-11-06 19:02:35 -0300

    initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import bd from "../images/bd.gif"
+import photo1 from "../images/1.jpg"
+import photo2 from "../images/2.jpg"
+import photo3 from "../images/3.jpg"
+import photo4 from "../images/4.jpg"
+import photo5 from "../images/5.jpg"
+import photo6 from "../images/6.jpg"
+import photo7 from "../images/7.jpg"
+
+const photos = [photo1, photo2, photo3, photo4, photo5, photo6, photo7]
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <main>
+    <div id="carousel">
+      <Carousel
+        transitionTime={1000}
+        showThumbs={false}
+        infiniteLoop
+        useKeyboardArrows
+        autoPlay
+      >
+        {photos.map((p, i) => (
+          <div key={i}>
+            <img src={p} />
+          </div>
+        ))}
+      </Carousel>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+
+    <h1>HAPPY BIRTHDAY!!!</h1>
+    <br />
+    <span>🎂🎂🎂🎂🎂🎂🎂🎂🎂</span>
+    <br />
+    <img src={bd} />
+    <br />
+    <span>❤️❤️❤️❤️❤️❤️❤️❤️❤️</span>
+  </main>
 )
 
 export default IndexPage
